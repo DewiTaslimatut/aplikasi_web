18f3ee41b0014f6fa3977565d0850ac3348cc26a
DewiTaslimatut <dewitataslimatut@gmail.com>
2025-09-27 13:12:08 +0700

    CRUD tabel employee

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/employees', [EmployeeController::class, 'index']);
+Route::resource('employees', EmployeeController::class);
